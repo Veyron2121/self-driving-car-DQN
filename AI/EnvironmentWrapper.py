4f38f157d52bf943b63092bf0805c943af95b14d
Varun Lodaya <varunblodaya@gmail.com>
2020-07-19 09:48:34 +0400

    Change implementation of step in EnvironmentWrapper.py, and added the basic reward function.

--- AI/EnvironmentWrapper.py
+++ AI/EnvironmentWrapper.py
@@ -1,26 +1,33 @@
+import json
 import os
+import random
 from pathlib import Path
 
-from FrameBuffer import FrameBuffer
+import numpy as np
+import zmq
+import cv2
+
+from ActionSpace import Acc, Steer
+from .Frame import Frame
+from .FrameBuffer import FrameBuffer
 
 class EnvironmentWrapper:
 
     def __init__(self):
 
         # TODO: Change socket behaviour here - Varun
-        # initialise comms with the simulator here
-        sock = socket.socket(socket.AF_INET,
-                             socket.SOCK_STREAM)  # initialise the socket
-        # connect to localhost, port 2345
-        sock.bind(("127.0.0.1", 4444))
-        sock.listen(1)
         print("Waiting to connect to Simulator...")
-        self.clientsocket, _ = sock.accept()  # connect to simulator [BLOCKING]
+
+        context = zmq.Context()
+        # noinspection PyUnresolvedReferences
+        self.socket = context.socket(zmq.REP)
+        self.socket.bind("tcp://*:5555")
+
         print("Connected to simulator!")
-        # =========================================
+
 
         # initialising frame buffer
-        self.buffer_size = 4.  # could change this
+        self.buffer_size = 4  # could change this
 
         self.current_buff = FrameBuffer(
             size=self.buffer_size)  # this is the FrameBuffer that keeps track of the latest frames.
@@ -35,7 +42,17 @@ class EnvironmentWrapper:
 
         self.done = False
 
-        self.max_time_steps_per_episode = 500  # change this based on the enviorment
+        self.max_time_steps_per_episode = 500  # change this based on the environment
+
+        self.action_space = [Acc.BRAKE, Steer.TURN_RIGHT,
+                             Acc.BRAKE, Steer.DO_NOTHING,
+                             Acc.BRAKE, Steer.TURN_RIGHT.
+                             Acc.DO_NOTHING, Steer.TURN_RIGHT,
+                             Acc.DO_NOTHING, Steer.DO_NOTHING,
+                             Acc.DO_NOTHING, Steer.TURN_RIGHT,
+                             Acc.ACCELERATE, Steer.TURN_RIGHT,
+                             Acc.ACCELERATE, Steer.DO_NOTHING,
+                             Acc.ACCELERATE, Steer.TURN_RIGHT]
 
     def get_input_shape(self):
         """
@@ -53,16 +70,16 @@ class EnvironmentWrapper:
         """
         returns a random action to be taken. [steering, acceleration, brake]
         """
-        raise NotImplementedError
+        return random.choice(self.action_space)
 
-    def get_num_action_space(self):
+    def get_num_action_space(self) -> int:
         """
-        returns the number of permuations of valide actions. For Eg. [steer left, accelerate and no brake] is ONE action
+        returns the number of permuations of valid actions. For Eg. [steer left, accelerate and no brake] is ONE action
         [steer right, accelerate and brake] is invalid as we cannot accelerate and brake at the same time.
         there are 9 possible actions I think?
         """
         # TODO: Maybe make this more general?
-        return 9
+        return len(self.action_space)
 
     def reset(self):
         """
@@ -90,42 +107,24 @@ class EnvironmentWrapper:
         self.time_steps += 1
 
         if not self.is_done():
-            # if the episode has not ended
-            # =======================
-            # in this section of the code, we wait for the simulator for to give us an action request
-            # then we give it an action through the TCP/IP protocol
-            msg = self.receive_message()
-            if msg == 'requesting action':
-                # send the action
-                self.send_message('action'.encode())
-                # now wait for the action to be completed
-                action_done = self.receive_message()
+            action_string = str(action[0]) + ',' + str(action[1])
 
-                if action_done == 'action done':
-                    # the 4 images will be stored as scr{i}.png
-                    for i in range(1, 4):
-                        # each Frame object is then assigned to the FrameBuffer class in chronological order
-                        path = Path(os.getcwd()).parent / 'scr.{0}.png'.format(
-                            i)
-                        f = self.get_frame(path)
-                        self.current_buff.assign_to_buffer(f)
+            self.socket.send(action_string.encode('ascii'))
 
-                        angle, distance, speed = self.get_game_stats()
+            message = self.socket.recv()
+            # unpack JSON
+            data = json.loads(message)
+            # print("Received request: %s" % message)
+            speed, angle, distance = self.get_game_stats(data)
 
-                    # still need to figure out how to send over distance, speed angle data
+            f = self.get_frame(data["image_path"])
+            self.current_buff.assign_to_buffer(f)
 
-                    dist_delta = self.prev_dist - distance
-
-                    if abs(dist_delta) > 30:
-                        dist_delta = 5  # if there's too big a negative jump in the distance, the car has passed a checkpoint.
-                        # so, don't penalise it for that.
-
-                    # calculate reward based on the game stats
-                    reward = (dist_delta * 0.7) + (speed * 0.3) - (angle * 0.1)
+            reward = self.get_basic_reward(speed, angle, action)
 
             # this buffer tensor is what we will input to the NN in the next time step
             # we record this as the next observation.
-            buffer_tensor = self.current_buff.to_input_tensor()
+            buffer_tensor = self.current_buff.get_input_tensor()
             # A buffer is a collection of consecutive frames that we feed to the NN. These frames are already processed.
 
             # this returns the state of the environment after the action has been completed, the reward for the action and if the episode ended.
@@ -133,20 +132,37 @@ class EnvironmentWrapper:
         else:
             return None
 
-    def send_message(self, string):
-        try:
-            self.clientsocket.sendall(string.encode())
-        except:
-            print("Socket Exception while sending data to simulator")
+    def get_basic_reward(self, speed, angle, action):
+        v_min = 5
+        v_brake = 20
+        v_max = 18
+        phi_threshold = 0.05
+        reward = 0
+        acc = action[0]
+        steer = action[1]
 
-    def receive_message(self):
-        data = None
-        try:
-            data = self.clientsocket.recv(128).decode()
-        except:
-            print("Socket Exception while recieving data from simulator")
+        if speed > v_brake and acc == Acc.BRAKE:
+            reward += 1
+        if speed > v_brake and acc == Acc.ACCELERATE:
+            reward -= 1
+        if speed < v_max and acc == Acc.ACCELERATE:
+            reward += 1
+        if speed < v_max and acc == Acc.BRAKE:
+            reward -= 1
 
-        return data
+        if angle < -phi_threshold and steer == Steer.TURN_RIGHT:
+            reward += 1
+        if angle < -phi_threshold and steer == Steer.TURN_LEFT:
+            reward -= 1
+        if angle > phi_threshold and steer == Steer.TURN_LEFT:
+            reward += 1
+        if angle > phi_threshold and steer == Steer.TURN_RIGHT:
+            reward -= 1
+
+        if speed < v_min and acc != Acc.ACCELERATE:
+            reward -= 1
+
+        return reward
 
     def is_done(self):
         """
@@ -171,19 +187,16 @@ class EnvironmentWrapper:
         np_data = np.asarray(image, dtype="float32")
         return Frame(np_data)
 
-    def get_current_state():
+    def get_current_state(self):
         """
         get the last n frames from the simulator (they might be stored in a folder by the simulator)
         and store them in a buffer and return them
         """
         return self.current_buff
 
-    def get_game_stats(self):
+    def get_game_stats(self, data):
         """
         returns a tuple of angle, distance from checkpoint and speed from the sim. Again requires comms with simulator.
         """
-        self.send_message("requesting info")
-        string = self.receive_message()
-
-        # TODO: SOBI parse this JSON string which will contain game information and return
-
+        return data["velocity"], data["angle_from_road"], \
+               data["distance_from_road"]
